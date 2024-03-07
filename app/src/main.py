cd3c96c74ba4799071c3af9ccbeaa24d127d2126
ishaanjain1507 <ishaanjain1507@gmail.com>
2024-03-07 22:26:01 +0530

    added handlers

--- app/src/main.py
+++ app/src/main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
 import uvicorn
+
+from app.src.routers import dev_handler, user_handler
 # from methods import login, search
 app = FastAPI()
 
 @app.get('/')
 def login():
-    return login()
+    return {}
 
 @app.get('/search')
 def search():
@@ -16,3 +18,4 @@ def add_accounts():
     return 
 
 if __name__ == "__main__":
+    uvicorn.run(app, host='0.0.0.0', port=8000)
