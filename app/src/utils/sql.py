6a7ff63842b1af79a021f569b00632c5a9550752
Surya saketh Chakka <sakethchakka@MacBook-Air-2.local>
2024-03-09 13:03:20 +0530

    changed sql.py

--- app/src/utils/sql.py
+++ app/src/utils/sql.py
@@ -3,9 +3,9 @@ import scraping as scrap
 import app
 import pandas as pd
 
-# NAME,BlockingIOError = (LINK)
-data = pd.read_csv('alumni data - Form Responses 1.csv')
-data.fillna("", inplace = True)
+data = pd.read_csv('alumni data - Form Responses 1.csv', na_values="", keep_default_na=False).astype(str)
+data.fillna("", inplace=True)
+data.replace("nan", "", inplace=True)
 # for i,j in data.iterrows():
 #     print(j["Name"])
 mail = "chsuryasaketh@gmail.com"
@@ -21,9 +21,9 @@ name varchar(255),
 bio varchar(255),
 location varchar(255),
 contact varchar(255) primary key,
-branch varchar(255),
-year varchar(255),
-programme varchar(255)
+branch varchar(5),
+year varchar(3),
+programme varchar(5)
 );""")
 
 cur.execute("""create table if not exists personal_mails(
@@ -35,34 +35,48 @@ mail_id varchar(255)
 cur.execute("""create table if not exists numbers(
 id varchar(255) REFERENCES profile(contact)
     ON DELETE CASCADE,
-phone varchar(255)
+phone varchar(10)
 );""")
 
-# cur.execute("""create table if not exists jobs(
-# id varchar(255) primary key REFERENCES profile(contact)
-#     ON DELETE CASCADE,
-# ) """)
-for i,j in data.iterrows():
-    name, bio, location, contact_url, contact, jobs, institutes = scraper.scrape(j["linked in profile url"])
+cur.execute("""create table if not exists jobs(
+id varchar(255) REFERENCES profile(contact)
+    ON DELETE CASCADE,
+    post varchar(255),
+    company varchar(255)
+) """)
+
+cur.execute("""create table if not exists education(
+id varchar(255) REFERENCES profile(contact)
+    ON DELETE CASCADE,
+    college varchar(255),
+    degree varchar(255)
+) """)
+
+for i, j in data.iterrows():
+    name, bio, location, contact_url, contact, jobs_array, edu_array = scraper.scrape(j["linked in profile url"])
+    bio = bio[:255]
+
     # bio = app.edit_bio(bio)
-    roll = j["Roll number"].lower()
+    roll = j["Email Address"].lower().split('@')[0]
     branch = roll[:2]
     year = roll[2:4]
     programme = roll[4:9]
     email = j["personal mail"]
     phone1 = j["phone number "]
+    phone1 = phone1.replace(".0", "")
     print(phone1)
-    phone2 = j["Alternative phone number"]
-    cur.execute("""insert into profile values (%s, %s, %s, %s, %s, %s, %s)""", (name, bio, location, contact_url, branch, year, programme))
+    cur.execute("""insert into profile values (%s, %s, %s, %s, %s, %s, %s)""",
+                (name, bio, location, contact_url, branch, year, programme))
     if email != "":
         cur.execute("""insert into personal_mails values (%s, %s)""", (contact_url, email))
-    if phone1 != "":
+    if len(phone1) == 10:
         cur.execute("""insert into numbers values (%s, %s)""", (contact_url, phone1))
-    if phone2 != "":
-        cur.execute("""insert into numbers values (%s, %s)""", (contact_url, phone2))
-    # cur.execute("""select * from profile;""")
-# for row in cur.fetchall():
-#     print(row)
+    for job in jobs_array:
+        cur.execute("""insert into jobs values (%s, %s, %s)""",
+                    (contact_url, job[0], job[1]))
+    for edu in edu_array:
+        cur.execute("""insert into education values (%s, %s, %s)""",
+                    (contact_url, edu[0], edu[1]))
 
 con.commit()
 cur.close()
